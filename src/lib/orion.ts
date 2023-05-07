32d87d688c1ff144d6baa8707455a23f7c1a43cf
Kizito Akhilome <kizito@akhilo.me>
2023-05-07 15:49:22 +0100

    make joi a dependency

--- src/lib/orion.ts
+++ src/lib/orion.ts
@@ -6,7 +6,7 @@ import { gatherRoutes } from './gather-routes';
 import { generateRouter } from './route-generator';
 import { routeLogger } from './route-logger';
 import { OrionOptions } from './types';
-import { defaultOptions, validatePeerDeps } from './utils';
+import { defaultOptions } from './utils';
 
 export function orion(opts?: OrionOptions) {
   opts = { ...defaultOptions, ...opts };
@@ -14,9 +14,6 @@ export function orion(opts?: OrionOptions) {
   const callerExt = path.extname(callSite).split('.')[1];
   const suffix = opts.suffix as string;
   const ext = opts.ext || callerExt;
-
-  // ensure required dependencies are installed
-  validatePeerDeps(opts);
 
   const paths = fg.sync([`./**/*.${suffix}.${ext}`], {
     absolute: true,
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import fs from 'fs';
-import path from 'path';
-import { PackageJson } from 'type-fest';
-import { OrionOptions, RequestHandler } from './types';
+import { RequestHandler } from './types';
 
 export const defaultOptions = {
   suffix: 'route',
   base: '',
   validation: {
-    enabled: true,
+    enabled: false,
     options: { abortEarly: false, stripUnknown: true },
     errorStatusCode: 422,
   },
@@ -16,29 +13,6 @@ export const defaultOptions = {
     enabled: true,
   },
 } as const;
-
-export function validatePeerDeps(options: OrionOptions): void {
-  const { error: logError } = console;
-  const missingDeps: string[] = [];
-  try {
-    const f = fs.readFileSync(path.join(process.cwd(), 'package.json'), { encoding: 'utf-8' });
-    const pkg = JSON.parse(f) as PackageJson;
-
-    const hasJoi = Boolean(pkg.dependencies?.joi);
-
-    if (options.validation?.enabled && !hasJoi) {
-      missingDeps.push('joi');
-    }
-  } catch (error) {
-    logError(
-      '[orion]: WARN: unable to validate required dependencies for orion; ensure all required peer dependencies are installed.',
-    );
-  }
-
-  if (missingDeps.length) {
-    throw new Error(`[orion]: missing required dependencies - ${missingDeps.join(', ')}`);
-  }
-}
 
 export enum HttpMethod {
   GET = 'GET',
